690b41a5b8e365b96b86c6320eb7b6d0ee8f6f1b
Avadhut Shinde <105962383+avdhut9@users.noreply.github.com>
2022-11-10 08:16:43 +0530

    Update App.js

--- frontEnd/my-app/src/App.js
+++ frontEnd/my-app/src/App.js
@@ -8,6 +8,7 @@ import {
   Code,
   Grid,
   theme,
+  Container
 } from '@chakra-ui/react';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
 import { Logo } from './Logo';
@@ -16,9 +17,11 @@ import AllRoutes from './Routes/AllRoutes';
 function App() {
   return (
     <ChakraProvider theme={theme}>
+     <Container maxW="100%" p="0px">
       {/* <Navbar/> */}
         <AllRoutes/>
       {/* <Footer/> */}
+     </ConTainer>
     </ChakraProvider>
   );
 }
