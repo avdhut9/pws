916674a882e584590b7284d0c1571802e95d68d0
Avadhut Shinde <105962383+avdhut9@users.noreply.github.com>
2022-11-10 08:23:31 +0530

    Add files via upload

--- frontEnd/my-app/src/pages/Home/homepage.jsx
+++ frontEnd/my-app/src/pages/Home/homepage.jsx
@@ -1,10 +1,10 @@
-import { Container } from "@chakra-ui/react";
+import { Box, Container } from "@chakra-ui/react";
 import Home from "../../components/Home/Home";
-
+// 
 export default function Homepage(){
    return(
-    <Container>
-    {/* <Home/> */}
-</Container>
+    <Box w="100%"  >
+    <Home/>
+</Box>
    )
 }
