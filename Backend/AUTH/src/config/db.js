c5eb065f5779c3b7d6c6105b4ab3b3bd3702e63c
Nayan Kumar <naayaankumar@gmail.com>
2022-11-12 22:52:09 +0530

    updaetd

--- Backend/AUTH/src/config/db.js
+++ Backend/AUTH/src/config/db.js
@@ -1,5 +1,5 @@
 const mongoose= require("mongoose")
 const connect = async()=>{
-    return mongoose.connect(process.env.DB_URL)
+    return mongoose.connect("mongodb+srv://NAYAN:NAYAN@cluster0.u60zxbv.mongodb.net/Project?retryWrites=true&w=majority")
 }
 module.exports = connect
