117f0fa2e2f7fc66c602a612dec59143b691c712
Nayan Kumar <naayaankumar@gmail.com>
2022-11-12 10:00:45 +0530

    updated routes

--- frontEnd/my-app/src/Routes/AllRoutes.jsx
+++ frontEnd/my-app/src/Routes/AllRoutes.jsx
@@ -9,9 +9,9 @@ import Homepage from "../pages/Home/homepage";
 export default function AllRoutes(){
    return( 
    <Routes>
-    <Route path="/" element={<Homepage/>}/>
-    <Route path="/login" element={<Login/>}></Route>
-    <Route path="/signup" element={<SignUp/>}></Route>
+      <Route path="/" element={<Homepage/>}/>
+      <Route path="/login" element={<Login/>}></Route>
+      <Route path="/signup" element={<SignUp/>}></Route>
    </Routes>
    )
 
