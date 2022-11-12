c535e004364af214d4b8b6f1b70dc22ec7c0127b
satyaprakash singh <satyamprakash448Qgmail.com>
2022-11-13 00:38:06 +0530

    Clean Code

--- frontEnd/my-app/src/Routes/AllRoutes.jsx
+++ frontEnd/my-app/src/Routes/AllRoutes.jsx
@@ -3,8 +3,10 @@
 import { Route, Routes } from "react-router-dom";
 import Login from "../components/Auth/Login";
 import SignUp from "../components/Auth/Signup";
-import Maintable from "../components/Table/Maintable";
+// import Maintable from "../components/Table/Maintable";
+
 import Homepage from "../pages/Home/homepage";
+import ProductRout from "../pages/Product/Rout/ProductRout";
 
 
 export default function AllRoutes(){
@@ -13,6 +15,9 @@ export default function AllRoutes(){
     <Route path="/" element={<Homepage/>}/>
     <Route path="/login" element={<Login/>}></Route>
     <Route path="/signup" element={<SignUp/>}></Route>
+    <Route path="/product" element={<ProductRout/>}></Route>
+    {/* <Route path="/workspace" element={<Maintable/>}></Route> */}
+
    </Routes>
    )
 
---
+++ frontEnd/my-app/src/pages/Product/Rout/ProductRout.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+// import Maintable from '../../../components/Table/Maintable'
+import  Navigation from "../Sidebar/Navigation"
+
+
+
+const ProductRout = () => {
+  return (
+    <div>
+        <Navigation/>
+     
+    </div>
+  )
+}
+
+export default ProductRout
